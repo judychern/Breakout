a9c9878f0561345560991cce99913f7ac48d97ba
judychern <chern.judy@gmail.com>
2015-07-11 14:34:19 -0700

    Added files

---
+++ box.js
@@ -0,0 +1,11 @@
+var boundingBox = function(x,y,width,height){
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	};
+	
+boundingBox.prototype.drawBox = function(boundingBox){
+	ctx.fillRect(this.x,this.y,this.width,this.height);
+};
+
