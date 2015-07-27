16c4f3c64a3321f6293b6a6260fa80ece551d577
judychern <chern.judy@gmail.com>
2015-07-26 18:27:29 -0700

    refactoring with plane normal

--- game.js
+++ game.js
@@ -36,7 +36,7 @@ Game.prototype.renderBricks  = function () {
   var brick = this.objects[i];
   brick.drawBox(brick.x, brick.y, brick.width, brick.height);
  }
-}
+};
 
 	
 	
@@ -68,6 +68,7 @@ Game.prototype.drawScreen = function() {
     game1.walls.drawWalls(this.walls);
     game1.ball.drawBall_start(this.ball);
     game1.paddle.draw();
+	game1.renderBricks();
 	
 	if(detect_collide(game1.walls.right)){
 		console.log("BOOM!");
@@ -113,21 +114,16 @@ Game.prototype.gameLoop = function() {
 		if(collisionDirection==="top"){
 			handle_collision(game1.ball,collidableObject,collidableObject.planeTop);
 		}
+		if(collisionDirection==="bottom"){
+			handle_collision(game1.ball,collidableObject,collidableObject.planeBottom);
+		}
+		if(collisionDirection==="left"){
+			handle_collision(game1.ball,collidableObject,collidableObject.planeLeft);
+		}
+		if(collisionDirection==="right"){
+			handle_collision(game1.ball,collidableObject,collidableObject.planeRight);
+		}
 	}
-	//if (detect_collide(game1.walls.top)){
-	//	handle_collision(game1.ball,game1.walls.top);
-	//}
-	
-	//if(detect_collide(game1.walls.left)){
-	//	handle_collision(game1.ball,game1.walls.left);
-	//}
-	
-	//if(detect_collide(game1.walls.right)){
-	//	handle_collision(game1.ball,game1.walls.right);
-	//}
-	//objects - add walls and bricks
-	
-	//ball has not hit the paddle
 	game1.drawScreen();
 };
 
@@ -145,76 +141,118 @@ function startGame(event){
     }
 };
 
-function detect_collide(boundingBox){
-		var ballLeft = game1.ball.boundingCircle.x;
-		var ballRight = game1.ball.boundingCircle.x + game1.ball.boundingCircle.width;
-		var ballTop = game1.ball.boundingCircle.y;
-		var ballBottom = game1.ball.boundingCircle.y + game1.ball.boundingCircle.height;
-		var boundingBoxLeft = boundingBox.x;
-		var boundingBoxRight = boundingBox.x + boundingBox.width;
-		var boundingBoxTop = boundingBox.y;
-		var boundingBoxBottom = boundingBox.y + boundingBox.height;
-		var velX = game1.ball.velocity.x;
-		var velY = game1.ball.velocity.y;
-		var overlapX, overlapY;
+function detect_collide(boundingBox)
+{
+	var ballLeft = game1.ball.boundingCircle.x;
+	var ballRight = game1.ball.boundingCircle.x + game1.ball.boundingCircle.width;
+	var ballTop = game1.ball.boundingCircle.y;
+	var ballBottom = game1.ball.boundingCircle.y + game1.ball.boundingCircle.height;
+	var boundingBoxLeft = boundingBox.x;
+	var boundingBoxRight = boundingBox.x + boundingBox.width;
+	var boundingBoxTop = boundingBox.y;
+	var boundingBoxBottom = boundingBox.y + boundingBox.height;
+	var velX = game1.ball.velocity.x;
+	var velY = game1.ball.velocity.y;
+	var overlapX, overlapY;
 		
-		var collide = (game1.ball.boundingCircle.x < boundingBox.x + boundingBox.width &&
-		game1.ball.boundingCircle.x + game1.ball.boundingCircle.width > boundingBox.x &&
-		game1.ball.boundingCircle.y < boundingBox.y + boundingBox.height &&
-		game1.ball.boundingCircle.y + game1.ball.boundingCircle.height > boundingBox.y);
-		if (collide){
-			console.log("This is a collision");
-		if (velx === 0 && velY > 0){
+	var collide = (game1.ball.boundingCircle.x < boundingBox.x + boundingBox.width &&
+	game1.ball.boundingCircle.x + game1.ball.boundingCircle.width > boundingBox.x &&
+	game1.ball.boundingCircle.y < boundingBox.y + boundingBox.height &&
+	game1.ball.boundingCircle.y + game1.ball.boundingCircle.height > boundingBox.y);
+	
+	if (collide)
+	{
+		console.log("This is a collision");
+	
+		if (velX === 0 && velY > 0)
+		{
+			return "top";
+		} 
+		else if (velX === 0 && velY < 0)
+		{
+			return "bottom";
+		} 
+		else if (velX > 0 && velY === 0)
+		{
+			return "left";
+		} 
+		else if (velX < 0 && velY === 0) 
+		{
+			return right;
+		} 
+		else if (velX > 0 && velY > 0) 
+		{
+		//top or left
+		overlapX = ballRight - boundingBoxLeft;
+		overlapY = ballBottom - boundingBoxTop;
+		if (overlapX > overlapY) 
+		{
+			return "top";
+		} 
+		else 
+		{
+			return "left";
+		}
+	} 
+	else if (velX < 0 && velY > 0) 
+	{
+	//"top or right"
+	overlapX = boundingBoxRight - ballLeft;
+	overlapY = ballBottom - boundingBoxTop;
+	if (overlapX > overlapY) 
+	{
 		return "top";
-		} else if (velX === 0 && velY < 0){
-		// bottom
-		} else if (velX > 0 && velY === 0){
-		// left 
-		} else if (velX < 0 && velY === 0) {
-		// right
-		} else if (velX > 0 && velY > 0) }{
-		// top or left
-		overlapX = ballRight - colObjectLeft;
-		overlapY = ballBottom - colObjectTop;
-		if (overlapX > overlapY) {
-		// top
-		} else {
-		// left
-		}
-		} else if (velX < 0 && velY > 0) }{
-		// top or right
-		overlapX = colObjectRight - ballLeft;
-		overlapY = ballBottom - colObjectTop;
-		if (overlapX > overlapY) {
-		// top
-		} else {
-		// right
-		}
-		} else if (velX > 0 && velY < 0) }{
-		// bottom or left
-		overlapX = ballRight - colObjectLeft;
-		overlapY = colObjectBottom - ballTop;
-		if (overlapX > overlapY) {
-		// bottom
-		} else {
-		// left
-		}
-		} else if (velX < 0 && velY < 0) }{
-		// bottom or right
-		overlapX = colObjectRight - ballLeft;
-		overlapY = colObjectBottom - ballTop;
-		if (overlapX > overlapY) {
-		// bottom
-		} else {
-		// right
-		}
-		}
-				return true;
-			}
-			else {
-				return false;
-			}
-		};
+	} 
+	else 
+	{
+		return "right";
+	}
+} 
+else if (velX > 0 && velY < 0) 
+ //<-- unmatched
+{
+	//bottom or left
+	overlapX = ballRight - boundingBoxLeft;
+	overlapY = boundingBoxBottom - ballTop;
+	if (overlapX > overlapY) 
+	{
+		return "bottom";
+	} 
+	else 
+	{
+		return "left";
+	}
+} 
+else if (velX < 0 && velY < 0) 
+// <-- unmatched
+{
+	//bottom or right
+	overlapX = boundingBoxRight - ballLeft;
+	overlapY = boundingBoxBottom - ballTop;
+	if (overlapX > overlapY) 
+	{
+		return "bottom";
+	} 
+	else 
+	{
+		return "right";
+	}
+}
+
+
+
+}
+else
+{
+	return false;
+}
+};
+		
+		
+				
+			
+			
+		
 
 function handle_collision(ball, box, plane){
 	var ballPosition = new Vector ((ball.center.x + (ball.velocity.x*-1)), ball.center.y + (ball.velocity.y*-1));
